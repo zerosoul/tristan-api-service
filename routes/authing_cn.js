056e43763124d2bf8be2b7f1e463ca960e0c8521
Tristan <yanggc888@163.com>
2021-03-23 15:32:07 +0800

    feat: authing udf put route

--- routes/authing_cn.js
+++ routes/authing_cn.js
@@ -6,7 +6,7 @@ const managementClient = new ManagementClient({
 });
 // uid:603776f87f8900ee938a6419
 // authing 相关的接口
-const authingRoute = {
+const udfGetRoute = {
   method: 'GET',
   path: '/service/authing/{username}/udf/{key?}',
   handler: async ({ params }, h) => {
@@ -52,4 +52,55 @@ const authingRoute = {
     });
   },
 };
-module.exports = [authingRoute];
+const appendVeraHistoryRoute = {
+  method: 'PUT',
+  path: '/service/authing/{username}/udf/vera',
+  handler: async ({ params, payload }, h) => {
+    const { username = '' } = params;
+    try {
+      const currUser = await managementClient.users.find({ username });
+      console.log({ username, currUser, payload });
+      if (!currUser) {
+        return h.response({
+          code: -1,
+          data: null,
+          msg: '用户不存在',
+        });
+      }
+      const udfData = await managementClient.users.getUdfValue(currUser.id);
+      console.log({ udfData });
+      let key = 'vera';
+      let tmp = null;
+      try {
+        tmp = JSON.parse(udfData[key]);
+      } catch (error) {
+        tmp = [];
+      }
+      tmp.push(payload);
+      try {
+        await managementClient.users.setUdfValue(currUser.id, { [key]: JSON.stringify(tmp) });
+        return h.response({
+          code: 0,
+          data: tmp,
+          msg: '追加成功',
+        });
+      } catch (error) {
+        let { code, message } = error;
+        return h.response({
+          code,
+          data: null,
+          msg: message,
+        });
+      }
+    } catch (error) {
+      console.log({ error });
+      let { code, message } = error;
+      return h.response({
+        code,
+        data: null,
+        msg: message,
+      });
+    }
+  },
+};
+module.exports = [udfGetRoute, appendVeraHistoryRoute];
